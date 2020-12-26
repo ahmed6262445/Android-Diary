286402152cb6686098f0aee6e00034ea6065f460
Ahmet <01-131172-004@student.bahria.edu.pk>
2020-12-26 22:19:46 +0500

    Some fixes

--- app/src/main/java/com/example/diary/Login.java
+++ app/src/main/java/com/example/diary/Login.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,7 +19,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
 
         initializeComponents();
     }
