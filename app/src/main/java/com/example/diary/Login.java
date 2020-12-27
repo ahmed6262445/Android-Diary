95d9fd4ec0b5b728541365effdee18ceaba20045
Ahmet <01-131172-004@student.bahria.edu.pk>
2020-12-28 02:34:26 +0500

    Login complete

--- app/src/main/java/com/example/diary/Login.java
+++ app/src/main/java/com/example/diary/Login.java
@@ -1,22 +1,33 @@
 package com.example.diary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import static android.content.ContentValues.TAG;
 
 public class Login extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
 
     Button btnLogin;
     Button btnSignup;
     Button btnForget;
 
-    TextInputLayout tilUsername;
+    TextInputLayout tilEmail;
     TextInputLayout tilPassword;
 
     @Override
@@ -28,6 +39,8 @@ public class Login extends AppCompatActivity {
     }
 
     public void initializeComponents() {
+        this.firebaseAuth = FirebaseAuth.getInstance();
+
         this.btnLogin = (Button) findViewById(R.id.btn_login);
         this.btnSignup = (Button) findViewById(R.id.btn_signup);
         this.btnForget = (Button) findViewById(R.id.btn_forget);
@@ -36,24 +49,53 @@ public class Login extends AppCompatActivity {
         this.btnSignup.setOnClickListener(goToSignupPage);
         this.btnForget.setOnClickListener(goToForgetPasswordPage);
 
-        this.tilUsername = (TextInputLayout) findViewById(R.id.til_username);
+        this.tilEmail = (TextInputLayout) findViewById(R.id.til_email);
         this.tilPassword = (TextInputLayout) findViewById(R.id.til_password);
+    }
+
+    // Functions
+    public void login() {
+        String email = this.tilEmail.getEditText().getText().toString();
+        String password = this.tilPassword.getEditText().getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success");
+                    //FirebaseUser user = mAuth.getCurrentUser();
+
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+//                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                    Toast.makeText(getApplicationContext(), "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+
+//                    updateUI(null);
+                }
+            }
+        });
     }
 
     // Validations
     private boolean validateUsername() {
-        String value = this.tilUsername.getEditText().getText().toString().trim();
+        String value = this.tilEmail.getEditText().getText().toString().trim();
         String checkspaces = "\\A\\w{1,20}\\z";
 
         boolean isValid = true;
 
         if (value.isEmpty()) {
-            this.tilUsername.setError("Field can not be empty");
+            this.tilEmail.setError("Field can not be empty");
             isValid = false;
         }
         else {
-            this.tilUsername.setError(null);
-            this.tilUsername.setErrorEnabled(false);
+            this.tilEmail.setError(null);
+            this.tilEmail.setErrorEnabled(false);
             isValid = true;
         }
         return isValid;
@@ -101,6 +143,8 @@ public class Login extends AppCompatActivity {
             if(!validateUsername() | !validatePassword()) {
                 return;
             }
+
+            login();
         }
     };
 }
