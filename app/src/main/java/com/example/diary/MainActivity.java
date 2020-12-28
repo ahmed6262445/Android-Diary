256b3324ba6f2c1b8c568f82c15468233031595f
Ahmet <01-131172-004@student.bahria.edu.pk>
2020-12-28 05:29:57 +0500

    Fragments completely working

--- app/src/main/java/com/example/diary/MainActivity.java
+++ app/src/main/java/com/example/diary/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.diary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
 public class MainActivity extends AppCompatActivity {
+     ChipNavigationBar navMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeComponents();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+    }
+
+    public void initializeComponents() {
+        this.navMenu = (ChipNavigationBar) findViewById(R.id.nav_menu);
+        this.navMenu.setItemSelected(R.id.nav_menu_home, true);
+        navMenuOptions();
+    }
+
+    public void  navMenuOptions() {
+        this.navMenu.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+
+                switch (i) {
+                    case R.id.nav_menu_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.nav_menu_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            }
+        });
     }
 }
